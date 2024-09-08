b826b97aee4bb5f11c33c08207349b4fec646e69
N0tTryHard <billy.bones1@mail.ru>
2024-09-08 10:52:01 +0300

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# pks_1
+
+A new Flutter project.
+
+## Практическая работа 1
---
+++ android/app/src/main/kotlin/example/com/pks_1/MainActivity.kt
@@ -0,0 +1,5 @@
+package example.com.pks_1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
